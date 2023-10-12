4d31e8a33a7517585ae8de882ddd093dac3baffe
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2023-10-12 14:41:34 +0200

    wip main

--- src/main/java/sio/groupD/Main.java
+++ src/main/java/sio/groupD/Main.java
@@ -1,24 +1,57 @@
 package sio.groupD;
 
+import sio.tsp.TspConstructiveHeuristic;
+import sio.tsp.TspData;
+import sio.tsp.TspTour;
+
 public final class Main {
-  public static void main(String[] args) {
-    // TODO
-    //  - Renommage du package ;
-    //  - Implémentation des classes NearestNeighbor et DoubleEndsNearestNeighbor ;
-    //  - Affichage des statistiques dans la classe Main ;
-    //  - Documentation abondante des classes comprenant :
-    //    - la javadoc, avec auteurs et description des implémentations ;
-    //    - des commentaires sur les différentes parties de vos algorithmes.
 
-    // Longueurs optimales :
-    // att532 : 86729
-    // rat575 : 6773
-    // rl1889 : 316536
-    // u574   : 36905
-    // u1817  : 57201
-    // vm1748 : 336556
+    private record TspMetaData (String name, int optimalLength) { }
+    private record TspObservation(/*TspMetaData metaData, */TspConstructiveHeuristic algorithm, TspTour tour, long runTime) { }
 
-    // Exemple de lecture d'un jeu de données :
-    // TspData data = TspData.fromFile("data/att532.dat");
-  }
+    public static void main(String[] args) {
+        // TODO
+        //  - Implémentation des classes NearestNeighbor et DoubleEndsNearestNeighbor ;
+        //  - Affichage des statistiques dans la classe Main ;
+        //  - Documentation abondante des classes comprenant :
+        //    - la javadoc, avec auteurs et description des implémentations ;
+        //    - des commentaires sur les différentes parties de vos algorithmes.
+
+        //todo convert both names and optimalLengths table
+        //TspMetaData[] metaDatas = {new TspMetaData("att532", 2)};
+
+        String[] names = {"att532", "rat575", "rl1889", "u574", "u1817", "vm1748"};
+        int[] optimalLengths = {86729, 6773, 316536, 36905, 57201, 336556};
+        for (String name : names) {
+            TspData data = null;
+            try {
+                data = TspData.fromFile("data/" + name + ".dat");
+            } catch (Exception e) {
+                System.out.println("Something wrong happened while trying to read data from file : " + e);
+                return;
+            }
+
+            TspConstructiveHeuristic[] algorithms = {new NearestNeighbor(), new DoubleEndsNearestNeighbor()};
+            for (TspConstructiveHeuristic algorithm : algorithms) {
+                int nbrTours = data.getNumberOfCities();
+                TspObservation[] observations = new TspObservation[nbrTours];
+                for (int i = 0; i < nbrTours; i++) {
+                    long start = System.nanoTime();
+                    TspTour tour = algorithm.computeTour(data, i);
+                    long end = System.nanoTime();
+                    // (end - start) / 1000000 + "ms"
+                    observations[i] = new TspObservation(algorithm, tour, end - start);
+                }
+
+                TspObservation minTour = observations[0];
+                for (TspObservation observation : observations) {
+                    if (observation.tour.length() < minTour.tour.length()) {
+                        minTour = observation;
+                    }
+                }
+
+                System.out.println("min Tour pour " + name + " avec " + algorithm.getClass() + " " + minTour);
+            }
+        }
+    }
 }
