266a3cb34bd98e55be2e24489641475fc54dcf1e
Simon <43808186+GuggisbergSimon@users.noreply.github.com>
2023-10-23 18:08:04 +0200

    clean up code + comments

--- src/main/java/sio/groupD/Main.java
+++ src/main/java/sio/groupD/Main.java
@@ -11,12 +11,11 @@ public final class Main {
 
     // TODO remove traces of MAX_VALUE_TEST for rendu
     private static final int MAX_VALUE_TEST = 1000;
-
     private static final int NS_2_MS = 1000000;
+    private static final int NBR_MEANINGFUL = 3;
 
     public static void main(String[] args) {
         // TODO
-        //  - Affichage des statistiques dans la classe Main ;
         //  - Documentation abondante des classes comprenant :
         //    - la javadoc, avec auteurs et description des implémentations ;
         //    - des commentaires sur les différentes parties de vos algorithmes.
@@ -30,15 +29,16 @@ public final class Main {
                 new TspMetaData("vm1748", 336556),
         };
         TspConstructiveHeuristic[] algorithms = {new NearestNeighbor(), new DoubleEndsNearestNeighbor()};
+        double[][][] summaryData = new double[algorithms.length][metaDatas.length][NBR_MEANINGFUL];
 
+        // First table header
         String formatString = "| %-6s | %-25s | %-9d | %-14f | %-8f | %-12f | %-11f |%n";
         String line =     "+--------+---------------------------+-----------+----------------+----------+--------------+-------------+%n";
         System.out.format(line);
         System.out.format("| file   | algorithm                 | minLength | averageLength  | minRatio | averageRatio | averageTime |%n");
         System.out.format(line);
 
-        double[][][] summaryData = new double[algorithms.length][metaDatas.length][3];
-
+        // Iterates over all files
         for (int dataIndex = 0; dataIndex < metaDatas.length; dataIndex++) {
             TspMetaData metaData  = metaDatas[dataIndex];
             TspData data = null;
@@ -49,6 +49,7 @@ public final class Main {
                 return;
             }
 
+            // Iterates over all algorithms
             for (int algoIndex = 0; algoIndex < algorithms.length; algoIndex++) {
                 TspConstructiveHeuristic algorithm = algorithms[algoIndex];
                 int nbrTours = data.getNumberOfCities();
@@ -58,6 +59,7 @@ public final class Main {
                     continue;
                 }
 
+                // Computes all possible tour
                 TspObservation[] observations = new TspObservation[nbrTours];
                 for (int j = 0; j < nbrTours; j++) {
                     long start = System.nanoTime();
@@ -80,13 +82,14 @@ public final class Main {
 
                 double average = (double) totalLength / observations.length;
 
-                // Saves meaningful summarized data
+                // Saves meaningful data per file per algorithm
                 summaryData[algoIndex][dataIndex] = new double[]{
                         (double) minTour.tour.length() / metaData.optimalLength,
                         average / metaData.optimalLength,
                         totalTime / observations.length
                 };
 
+                // Prints meaningful data per file per algorithm
                 System.out.format(formatString,
                         metaData.name,
                         algorithm.getClass().getSimpleName(),
@@ -100,26 +103,28 @@ public final class Main {
         }
 
         System.out.format(line);
+        System.out.println();
 
-        System.out.println("\n\n");
-
+        // Second table header
         formatString = "| %-25s | %-8f | %-12f | %-11f |%n";
         line =            "+---------------------------+----------+--------------+-------------+%n";
         System.out.format(line);
         System.out.format("| algorithm                 | minRatio | averageRatio | averageTime |%n");
+
         for (int algoIndex = 0; algoIndex < algorithms.length; algoIndex++) {
-            int nbrSummary = 3;
-            double[] summaryLine = new double[nbrSummary];
+            // Computes the average of meaningful data per algorithm
+            double[] summaryLine = new double[NBR_MEANINGFUL];
             for (int dataIndex = 0; dataIndex < metaDatas.length; dataIndex++) {
-                for (int i = 0; i < nbrSummary; i++) {
+                for (int i = 0; i < NBR_MEANINGFUL; i++) {
                     summaryLine[i] += summaryData[algoIndex][dataIndex][i];
                 }
             }
 
-            for (int i = 0; i < nbrSummary; i++) {
+            for (int i = 0; i < NBR_MEANINGFUL; i++) {
                 summaryLine[i] /= metaDatas.length;
             }
 
+            // Prints the average of meaningful data per algorithm
             System.out.format(formatString,
                     algorithms[algoIndex].getClass().getSimpleName(),
                     summaryLine[0],
@@ -127,6 +132,7 @@ public final class Main {
                     summaryLine[2]
             );
         }
+
         System.out.format(line);
     }
 }
